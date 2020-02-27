f73874d571703a027980089449cbc699187f757d
Subhadip Ghosh <subhadipghosh11@gmail.com>
2020-02-27 14:20:12 +0530

    Initial data-collection-service project added

---
+++ data-collection-service/src/test/java/org/subhadig/ams/datacollectionservice/DataCollectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.subhadig.ams.datacollectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
