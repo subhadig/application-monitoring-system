c45ead060e0228423bdeb388734a4307084791b5
Preetdeep Kumar <preetdeep.shrivastava@gmail.com>
2020-04-25 13:10:22 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nohup java -jar data-provider-service/target/data-provider-service-0.0.1-SNAPSHOT.jar > output.log &
+cd data-provider-service/target
+java -jar data-provider-service-0.0.1-SNAPSHOT.jar > output.log &
 
