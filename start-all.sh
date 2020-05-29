332ee249d1e91e57574adff85cf930dc67ed8a7c
Subhadip Ghosh <subhadipghosh11@gmail.com>
2020-05-29 17:55:54 +0530

    Start stop scripts updated with streaming service

--- start-all.sh
+++ start-all.sh
@@ -9,23 +9,43 @@ docker network create ams-net
 
 log "Packaging data provider service.."
 mvn clean package -f data-provider-service/pom.xml -DskipTests
-docker build -t data-provider-service:latest --build-arg jarfile="target/data-provider-service-*.jar" data-provider-service/
+docker build -t subhadig/data-provider-service:latest --build-arg jarfile="target/data-provider-service-*.jar" data-provider-service/
 
 log "Starting data provider service.."
-docker run -d --rm --network ams-net -p 8081:8080 --name provider data-provider-service:latest
+docker run -d --rm --network ams-net -p 8081:8080 --name provider subhadig/data-provider-service:latest
 
 log "Packaging database service.."
-docker build -t database-service:latest database-service/
+docker build -t subhadig/database-service:latest database-service/
 
 log "Starting database service.."
-docker run -d --rm --network ams-net -p 27017:27017 --name db -e MONGO_INITDB_ROOT_USERNAME=mongouser -e MONGO_INITDB_ROOT_PASSWORD=mongouser database-service:latest
+docker run -d --rm --network ams-net -p 27017:27017 --name db\
+                                                        -e MONGO_INITDB_ROOT_USERNAME=mongouser\
+                                                        -e MONGO_INITDB_ROOT_PASSWORD=mongouser\
+                                                        subhadig/database-service:latest
+
+log "Building data streaming service.."
+docker build -t subhadig/zookeeper:latest -f data-streaming-service/Dockerfile.zookeeper data-streaming-service/
+docker build -t subhadig/kafka:latest -f data-streaming-service/Dockerfile.kafka data-streaming-service/
+
+
+log "Starting data streaming service.."
+
+docker run -d --rm --network ams-net -p 2181:2181 --name zookeeper -e ALLOW_ANONYMOUS_LOGIN=yes subhadig/zookeeper:latest
+
+docker run -d --rm --network ams-net -p 9092:9092 -p 29092:29092 --name kafka\
+                                                               -e ALLOW_PLAINTEXT_LISTENER=yes\
+                                                               -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181\
+                                                               -e KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP=PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT\
+                                                               -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,PLAINTEXT_HOST://:29092\
+                                                               -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092,PLAINTEXT_HOST://localhost:29092\
+                                                               subhadig/kafka:latest
 
 log "Packaging data collection service.."
 mvn clean package -f data-collection-service/pom.xml -DskipTests
-docker build -t data-collection-service:latest --build-arg jarfile="target/data-collection-service-*.jar" data-collection-service/
+docker build -t subhadig/data-collection-service:latest --build-arg jarfile="target/data-collection-service-*.jar" data-collection-service/
 
 log "Starting data collection service.."
-docker run -d --rm --network ams-net -p 8082:8080 --name collector data-collection-service:latest
+docker run -d --rm --network ams-net -p 8082:8080 --name collector subhadig/data-collection-service:latest
 
 log "Printing the running Docker containers..\n\n"
 docker ps
--- stop-all.sh
+++ stop-all.sh
@@ -5,14 +5,18 @@ log()
     echo "\n[stop-all] $1"
 }
 
-log "Stoping data provider service.."
-docker stop provider
+log "Stoping data collector service.."
+docker stop collector
+
+log "Stoping data streaming service.."
+docker stop kafka
+docker stop zookeeper
 
 log "Stopping database service.."
 docker stop db
 
-log "Stoping data collector service.."
-docker stop collector
+log "Stoping data provider service.."
+docker stop provider
 
 log "Stopping network ams-net"
 docker network rm ams-net
